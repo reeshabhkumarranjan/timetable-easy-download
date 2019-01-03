8e32a7f3a39446ec3e9d37cf20f51a982a5c5451
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-01-03 19:55:04 +0530

    Handled permission-error

--- timetable.py
+++ timetable.py
@@ -3,25 +3,36 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 import os
 import ctypes
 
-url = "http://www.iiitd.ac.in/sites/default/files/docs/education/2019/Time%20Table-Add-Drop-Winter%202019V4.pdf"
 
-if os.path.exists("timetable.pdf"):
-    os.remove("timetable.pdf")
+def run():
+    url = "http://www.iiitd.ac.in/sites/default/files/docs/education/2019/Time%20Table-Add-Drop-Winter%202019V4.pdf"
 
-urllib.request.urlretrieve(url, "timetable.pdf")
+    if os.path.exists("timetable.pdf"):
+        os.remove("timetable.pdf")
 
-pageNumberToExtract = 5
-inputFile = PdfFileReader("timetable.pdf", "rb")
-outputFile = PdfFileWriter()
-pageToExtract = inputFile.getPage(pageNumberToExtract)
-outputFile.addPage(pageToExtract)
+    urllib.request.urlretrieve(url, "timetable.pdf")
 
-with open("timetable2.pdf", "wb") as f:
-    outputFile.write(f)
+    pageNumberToExtract = 5
+    inputFile = PdfFileReader("timetable.pdf", "rb")
+    outputFile = PdfFileWriter()
+    pageToExtract = inputFile.getPage(pageNumberToExtract)
+    outputFile.addPage(pageToExtract)
 
-if os.path.exists("timetable.pdf"):
-    os.remove("timetable.pdf")
+    with open("timetable2.pdf", "wb") as f:
+        outputFile.write(f)
 
-os.rename("timetable2.pdf", "timetable.pdf")
+    if os.path.exists("timetable.pdf"):
+        os.remove("timetable.pdf")
 
-ctypes.windll.user32.MessageBoxW(0,"Succesfully fetched the latest timetable for CSE 2nd Year!","Success",0)
+    os.rename("timetable2.pdf", "timetable.pdf")
+
+    ctypes.windll.user32.MessageBoxW(
+        0, "Succesfully fetched the latest timetable for CSE 2nd Year! Click \"OK\" to continue", "Success", 0)
+
+
+if __name__ == "__main__":
+    try:
+        run()
+    except PermissionError as pe:
+        ctypes.windll.user32.MessageBoxW(
+            0, "The file is open in some other program. Click OK to continue", "Error", 0)
